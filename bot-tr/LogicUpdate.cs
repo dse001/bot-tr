d3425f5229b39884b260d56bfd965f6a4bc6e5a6
dermi <dermi@DESKTOP-NQIA3NH>
2023-12-03 19:31:54 +0200

    Add project files.

---
+++ bot-tr/LogicUpdate.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace bot_tr
+{
+	public class LogicUpdate : BotHandler
+
+    {
+		public LogicUpdate(string botToken) : base(botToken)
+		{
+		}
+
+		//public async override Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken token)
+		//{
+			
+
+       // }
+	}
+}
